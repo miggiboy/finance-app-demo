96fafa0c5096da4f8630a97811508b638900c0c4
miggiboy <mustimov.m@gmail.com>
2019-10-13 21:52:55 +0600

    add responsive styles and format html

--- src/FinanceApp.vue
+++ src/FinanceApp.vue
@@ -1,25 +1,36 @@
 <template>
-    <div class="p-32">
-        <div class="flex items-center mb-12">
-            <h2 class="font-semibold uppercase tracking-wide mr-3">Budget amount: </h2>
-            <input type="text" v-model.number="totalFunds" class="total-funds border rounded px-3 outline-none">
+    <div class="container mx-auto px-12 py-5 md:py-16 md:px-32">
+        <div class="flex flex-col md:flex-row items-center mb-12">
+            <h2 class="font-semibold md:uppercase tracking-wide mr-3">
+                Budget amount:
+            </h2>
+            <input
+                type="text"
+                v-model.number="totalFunds"
+                class="total-funds border rounded px-3 outline-none"
+                autofocus
+            />
         </div>
 
         <div>
             <div class="mb-4">
                 <h3 class="font-semibold mb-2">Expenses:</h3>
-                <div v-for="(spending, index) in spendings" class="py-3 border-b">
-                    <div class="flex">
-                        <input type="text"
-                               v-model="spending.name"
-                               placeholder="Expense"
-                               class="spending-name px-2 outline-none"
-                               @input="onTypeSpendingName(index)">
+                <div v-for="(spending, index) in spendings" class="py-6 md:py-3 border-b">
+                    <div class="flex flex-col md:flex-row">
+                        <input
+                            type="text"
+                            v-model="spending.name"
+                            placeholder="Expense"
+                            class="spending-name px-2 outline-none mb-3 md:mb-0"
+                            @input="onTypeSpendingName(index)"
+                        />
 
-                        <input type="text"
-                               v-model.number="spending.size"
-                               placeholder="Amount"
-                               class="spending-size px-2 outline-none">
+                        <input
+                            type="text"
+                            v-model.number="spending.size"
+                            placeholder="Amount"
+                            class="spending-size px-2 outline-none"
+                        />
 
                         <div v-if="totalFunds" class="spending-as-percent-from-total-funds">
                             {{ (spending.size / totalFunds * 100).toFixed(1) }}%
@@ -29,12 +40,22 @@
             </div>
 
             <div class="mb-8">
-                <button type="button" @click="addSpending" class="add-spending px-4 py-1 bg-blue-500 rounded text-white">Add</button>
+                <button
+                    type="button"
+                    @click="addSpending"
+                    class="add-spending px-4 py-1 bg-blue-500 rounded text-white"
+                >
+                    Add
+                </button>
             </div>
 
             <div class="free-funds flex">
-                <div class="mr-5">Balance: {{ freeFunds }}</div>
-                <div v-if="totalFunds" class="free-funds-percent">{{ (freeFunds / totalFunds * 100).toFixed(1) }}%</div>
+                <div class="mr-5">
+                    Balance: {{ freeFunds }}
+                </div>
+                <div v-if="totalFunds" class="free-funds-percent">
+                    {{ (freeFunds / totalFunds * 100).toFixed(1) }}%
+                </div>
             </div>
         </div>
     </div>
